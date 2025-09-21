cf0153c92d1798b91bdefc128c168c6004e0e79e
havassy <havassyandras@gmail.com>
2025-09-21 09:52:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.ncei.noaa.gov',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  base: '/climate-app/',
+  build: {
+    outDir: 'dist'
   }
 })
